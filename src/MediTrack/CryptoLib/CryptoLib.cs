e0bc46a9246171c32c941f316af75801edaf3cb7
Guilherme Soares <guilhermesoares1970@gmail.com>
2023-11-30 23:07:41 +0000

    feat: Only encrypt json values.

--- src/MediTrack/CryptoLib/CryptoLib.cs
+++ src/MediTrack/CryptoLib/CryptoLib.cs
@@ -2,6 +2,9 @@ namespace CryptoLib;
 
 using System.Diagnostics;
 using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 public class Crypto
 {
@@ -9,32 +12,62 @@ public class Crypto
     /// <summary>
     /// Computes the <paramref name="data"/> Digital Signature using <paramref name="authKey"/>, 
     /// in order to provide authenticity.
-    /// Encrypts the <c>digital signature</c> and original <paramref name="data"/> with a <c>symmetric key</c> providing confidentiality.
+    /// Encrypts all the json <paramref name="data"/> values with a <c>symmetric key</c> providing confidentiality.
     ///
     /// </summary>
-    /// <param name="data"><c>data</c> you want to encrypt.</param>
+    /// <param name="data"><c>data</c> Json data that you want to encrypt.</param>
     /// <param name="encryptionKey"><c>encryptionKey</c> is the public key of the receiver in Pem format. So only the receiver can read. 
     /// the encrypted data</param>
     /// <param name="authKey"><c>authKey</c> is the sender's private key in Pem format. Used to provide authenticity.</param>
     /// <returns> 
-    /// byte[]? with the <paramref name="data"/> and a header that 
+    /// byte[]? with the encrypted <paramref name="data"/> and a header that 
     /// contains the <c>symmetric key</c> and <c>initialization vector</c> used to encrypt the <paramref name="data"/>. 
     /// The <c>header</c> was encrypted with the <paramref name="encryptionKey"/> so the sender is the only one (apart from us), with access to the shared key.
     /// </returns>
-    static public byte[]? Protect(byte[] data, string encryptionKey, string authKey)
+    static public byte[]? Protect(JsonNode data, string encryptionKey, string authKey)
     {
         try
         {
             using RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
             using Aes aes = Aes.Create();
             rsa.ImportFromPem(authKey);
-            byte[] hash = rsa.SignData(data, SHA256.Create());
+            byte[] hash = rsa.SignData(JsonSerializer.SerializeToUtf8Bytes(data), SHA256.Create());
 
             rsa.ImportFromPem(encryptionKey);
             aes.GenerateIV();
             aes.GenerateKey();
             byte[] symmetric = rsa.Encrypt([.. aes.Key, .. aes.IV], false);
-            return [.. symmetric, .. aes.EncryptCbc(hash.Concat(data).ToArray(), aes.IV)];
+
+            JsonNode patient = data["patient"]!;
+            JsonArray allergies = patient["knownAllergies"]!.AsArray();
+            JsonArray consultationRecords = patient["consultationRecords"]!.AsArray();
+
+            int i = 0;
+            foreach (var consultation in consultationRecords.ToList())
+            {
+                consultationRecords[i] = new JsonObject
+                {
+                    ["date"] = EncryptToBase64(consultation!["date"]!.ToString(), aes),
+                    ["medicalSpeciality"] = EncryptToBase64(consultation!["medicalSpeciality"]!.ToString(), aes),
+                    ["doctorName"] = EncryptToBase64(consultation!["doctorName"]!.ToString(), aes),
+                    ["practice"] = EncryptToBase64(consultation!["practice"]!.ToString(), aes),
+                    ["treatmentSummary"] = EncryptToBase64(consultation!["treatmentSummary"]!.ToString(), aes),
+                };
+                i++;
+            }
+            i = 0;
+            foreach (var allergie in allergies.ToList())
+            {
+                allergies[i] = EncryptToBase64(allergie!.ToString(), aes);
+                i++;
+            }
+
+            patient["name"] = EncryptToBase64(patient!["name"]!.ToString(), aes);
+            patient["sex"] = EncryptToBase64(patient!["sex"]!.ToString(), aes);
+            patient["dateOfBirth"] = EncryptToBase64(patient!["dateOfBirth"]!.ToString(), aes);
+            patient["bloodType"] = EncryptToBase64(patient!["bloodType"]!.ToString(), aes);
+
+            return [.. symmetric, .. hash, .. JsonSerializer.SerializeToUtf8Bytes(data)];
         }
         catch (Exception e)
         {
@@ -42,6 +75,8 @@ public class Crypto
             return null;
         }
     }
+
+
 
     /// <summary>
     /// Decrypts the <c>header</c> of the given <paramref name="data"/> with the provided <paramref name="decryptionKey"/>
@@ -60,6 +95,8 @@ public class Crypto
 
             // header is [symmetricKey, iv]
             byte[] header = rsa.Decrypt(data[..256], false);
+            // Skip header bytes
+            data = data[256..];
 
             const int HEADER_SIZE = 32 + 16; // Aes.Key size + Aes.IV size
             Debug.Assert(
@@ -67,9 +104,39 @@ public class Crypto
                 $"ERROR in Unprotect: header size is {header.Length} and should be {HEADER_SIZE}");
 
             aes.Key = header[..32];
-            byte[] iv = header[32..];
+            aes.IV = header[32..];
 
-            return aes.DecryptCbc(data[256..], iv);
+
+            JsonNode objectData = JsonNode.Parse(data[256..])!; // Skip hash bytes
+            JsonNode patient = objectData["patient"]!;
+
+            patient["name"] = DecryptFromBase64(patient!["name"]!.ToString(), aes);
+            patient["sex"] = DecryptFromBase64(patient!["sex"]!.ToString(), aes);
+            patient["dateOfBirth"] = DecryptFromBase64(patient!["dateOfBirth"]!.ToString(), aes);
+            patient["bloodType"] = DecryptFromBase64(patient!["bloodType"]!.ToString(), aes);
+
+            int i = 0;
+            JsonArray consultationRecords = patient["consultationRecords"]!.AsArray();
+            foreach (var consultation in consultationRecords.ToList())
+            {
+                consultationRecords[i] = new JsonObject
+                {
+                    ["date"] = DecryptFromBase64(consultation!["date"]!.ToString(), aes),
+                    ["medicalSpeciality"] = DecryptFromBase64(consultation!["medicalSpeciality"]!.ToString(), aes),
+                    ["doctorName"] = DecryptFromBase64(consultation!["doctorName"]!.ToString(), aes),
+                    ["practice"] = DecryptFromBase64(consultation!["practice"]!.ToString(), aes),
+                    ["treatmentSummary"] = DecryptFromBase64(consultation!["treatmentSummary"]!.ToString(), aes),
+                };
+                i++;
+            }
+            i = 0;
+            JsonArray allergies = patient["knownAllergies"]!.AsArray();
+            foreach (var allergie in allergies.ToList())
+            {
+                allergies[i] = DecryptFromBase64(allergie!.ToString(), aes);
+                i++;
+            }
+            return [.. data[..256], .. JsonSerializer.SerializeToUtf8Bytes(objectData)];
         }
         catch (Exception e)
         {
@@ -104,4 +171,7 @@ public class Crypto
             return false;
         }
     }
+
+    private static string EncryptToBase64(string value, Aes aes) => Convert.ToBase64String(aes.EncryptCbc(Encoding.UTF8.GetBytes(value), aes.IV));
+    private static string DecryptFromBase64(string value, Aes aes) => Encoding.UTF8.GetString(aes.DecryptCbc(Convert.FromBase64String(value), aes.IV));
 }
