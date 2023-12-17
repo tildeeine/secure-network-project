80f56ef1e5baa16f67792b7ec482613cbe2b3430
Guilherme Soares <guilhermesoares1970@gmail.com>
2023-12-17 21:05:30 +0000

    feat: Expose SignData operation in CryptoLib CLI to produce consultation record signatures

--- MediTrack/CryptoLib/Program.cs
+++ MediTrack/CryptoLib/Program.cs
@@ -1,12 +1,17 @@
-﻿using System.Text.Json.Nodes;
+﻿using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using CryptoLib;
 
 const string TOOL_NAME = "CryptoLib";
-const string HELP = @$"{TOOL_NAME} Commands: 
-    help
-    protect (input-file) (output-file) (encryptionKey) (authKey)
-    unprotect (input-file) (output-file) (decryptionKey)
-    check (input-file) (authKey)"; //?Is this enough for help, or should we maybe be more specific? Insetad of authKey for protect, (sender's private key) or (your private key), etc. 
+const string HELP = @$"{TOOL_NAME} Usage: 
+    help Displays help message
+    protect (input-file) (output-file) (encryptionKey-file) (authKey-file) Protects the given file.
+    unprotect (input-file) (output-file) (decryptionKey-file) Unprotects the given file.
+    sign (input-file) (authKey-file) Signs data and outputs the signed data in base64 format.
+    check (input-file) (authKey-file) Checks the integrity of the given file.";
+//?Is this enough for help, or should we maybe be more specific? Insetad of authKey for protect, (sender's private key) or (your private key), etc. 
 
 if (args.Length < 1)
 {
@@ -14,37 +19,31 @@ if (args.Length < 1)
     return;
 }
 
-byte[]? inputFile = null;
+byte[]? bytes = null;
 switch (args[0])
 {
     case "protect":
+        if (args.Length != 5)
+        {
+            DisplayHelp();
+            return;
+        }
+        Console.WriteLine($"Protecting file {args[1]}");
+        bytes = Crypto.Protect(JsonNode.Parse(File.ReadAllBytes(args[1]))!, File.ReadAllText(args[3]), File.ReadAllText(args[4]));
+        if (bytes is null)
+            return;
+        File.WriteAllBytes(args[2], bytes);
+        break;
     case "unprotect":
 
-        int length = args.Length;
-        if ((args[0] == "protect" && length != 5)
-            || (args[0] == "unprotect" && length != 4))
+        if (args.Length != 4)
         {
             DisplayHelp();
             return;
         }
+        Console.WriteLine($"Unprotecting file {args[1]}");
+        bytes = Crypto.Unprotect(File.ReadAllBytes(args[1]), File.ReadAllText(args[3]));
 
-        inputFile = ParseFile(args[1]);
-        if (inputFile is null)
-        {
-            DisplayHelp();
-            return;
-        }
-        byte[]? bytes = null;
-        if (args[0] == "protect")
-        {
-            Console.WriteLine($"Protecting file {inputFile}");
-            bytes = Crypto.Protect(JsonNode.Parse(inputFile)!, args[3], args[4]);
-        }
-        else
-        {
-            Console.WriteLine($"Unprotecting file {inputFile}");
-            bytes = Crypto.Unprotect(inputFile, args[3]);
-        }
         if (bytes is null)
             return;
         File.WriteAllBytes(args[2], bytes);
@@ -55,29 +54,23 @@ switch (args[0])
             DisplayHelp();
             return;
         }
-        inputFile = ParseFile(args[1]);
-        if (inputFile is null)
+        byte[] inputFile = File.ReadAllBytes(args[1]);
+        Console.WriteLine(Crypto.Check(inputFile[..256], inputFile[256..], File.ReadAllText(args[2])));
+        break;
+    case "sign":
+        if (args.Length != 3)
         {
             DisplayHelp();
             return;
         }
-        Console.WriteLine(Crypto.Check(inputFile[..256], inputFile[256..], args[2]));
+        JsonNode? node = JsonNode.Parse(File.ReadAllText(args[1]));
+        Debug.Assert(node is not null);
+
+        var signedData = Crypto.SignData(JsonSerializer.SerializeToUtf8Bytes(node), File.ReadAllText(args[2]));
+        Console.WriteLine(Convert.ToBase64String(signedData));
         break;
     case "help":
     default: DisplayHelp(); return;
-}
-
-byte[]? ParseFile(string inputFile)
-{
-    try
-    {
-        return File.ReadAllBytes(inputFile);
-    }
-    catch (FileNotFoundException)
-    {
-        Console.WriteLine($"Couldn't find file: \"{inputFile}\"");
-    }
-    return null;
 }
 
 void DisplayHelp()
