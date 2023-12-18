f24f5a69dd68b19a64e400498467ae037c4504fa
tildeeine <t1ld3me@gmail.com>
2023-12-18 20:38:44 +0000

    fix: Fixed dotnet download and app server init

--- vagrant/init-scripts/init-app-server.sh
+++ vagrant/init-scripts/init-app-server.sh
@@ -2,12 +2,14 @@
 
 # VMSETTINGS: Adapter1, internal network, sw-1, promiscuous mode allow VMs. 
 
-sudo ifconfig eth0 192.168.0.20/24 up
+# Configure the network interfaces
+sudo ip addr add 192.168.0.20/24 dev eth0
+sudo ip link set dev eth0 up
 
 # Set External Firewall as default gateway
-sudo ip route add default via 192.168.0.10
+# sudo ip route add default via 192.168.0.10
 
 # Restart to apply changes
-sudo systemctl restart NetworkManager
+sudo systemctl restart systemd-networkd
 
 # ! edit the corresponding /etc/network/interfaces file.
