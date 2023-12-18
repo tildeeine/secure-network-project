1ae28958d188c700d4ae7206436b985e3796ec86
tildeeine <t1ld3me@gmail.com>
2023-12-18 12:07:47 +0000

    Fix: Minor fixes shell scripts and vagrantfile

--- vagrant/init-scripts/init-client-machine.sh
+++ vagrant/init-scripts/init-client-machine.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-# Update package repositories
-sudo apt-get update
-
-# Install .NET SDK
-sudo apt-get install -y dotnet-sdk-5.0
-
-# Install iptables
-sudo apt-get install -y iptables 
-sudo apt-get install -y iptables-persistent
-
-# Configure NAT
-iptables -t nat -F
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-# Make iptables rules persistent
-sudo sh -c 'iptables-save > /etc/iptables/rules.v4'
-
-# Ensure netfilter-persistent service is enabled
-sudo systemctl enable netfilter-persistent
-
-# Enable iptables service on boot
-sudo systemctl start netfilter-persistent
-
-# Restart networking service to apply changes
-sudo systemctl restart networking
+# All relevant commands now run in Vagrantfile
 
 #? Should we set up a connection to app server?
--- vagrant/init-scripts/init-fw.sh
+++ vagrant/init-scripts/init-fw.sh
@@ -10,11 +10,8 @@ sudo ifconfig eth1 192.168.1.19/24 up # Database server
 sudo sysctl net.ipv4.ip_forward=1
 
 # Set up NAT to let AS access the internet
-sudo iptables -t nat -F            # Flushes all the rules from table NAT
-sudo iptables -t nat -A POSTROUTING  -o eth2 -j MASQUERADE    # Creates a source NAT on interface eth2
-
-# Restart to apply changes
-sudo systemctl restart NetworkManager
+sudo /sbin/iptables -t nat -F            # Flushes all the rules from table NAT
+sudo /sbin/iptables -t nat -A POSTROUTING  -o eth2 -j MASQUERADE    # Creates a source NAT on interface eth2
 
 # ! edit the corresponding /etc/network/interfaces file.
 
@@ -22,31 +19,31 @@ sudo systemctl restart NetworkManager
 
 # Set up firewall rules
 # Flush existing rules and set the default policies
-sudo iptables -F
-sudo iptables -P INPUT DROP
-sudo iptables -P FORWARD DROP
-sudo iptables -P OUTPUT ACCEPT
+sudo /sbin/iptables -F
+sudo /sbin/iptables -P INPUT DROP
+sudo /sbin/iptables -P FORWARD DROP
+sudo /sbin/iptables -P OUTPUT ACCEPT
 
 # Allow loopback interface
-sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A OUTPUT -o lo -j ACCEPT
+sudo /sbin/iptables -A INPUT -i lo -j ACCEPT
+sudo /sbin/iptables -A OUTPUT -o lo -j ACCEPT
 
 # Allow SSH and HTTP connections from external machines (Auth and Client)
-sudo iptables -A INPUT -i eth2 -p tcp --dport 22 -j ACCEPT
-sudo iptables -A INPUT -i eth2 -p tcp --dport 80 -j ACCEPT
+sudo /sbin/iptables -A INPUT -i eth2 -p tcp --dport 22 -j ACCEPT
+sudo /sbin/iptables -A INPUT -i eth2 -p tcp --dport 80 -j ACCEPT
 
 # Redirect all HTTP connections to the App server
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 192.168.0.20:80
+sudo /sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 192.168.0.20:80
 
 # Allow SSH connections from App server to the database
-sudo iptables -A FORWARD -i eth0 -o eth1 -s 192.168.0.20 -d 192.168.1.30 -p tcp --dport 22 -j ACCEPT
+sudo /sbin/iptables -A FORWARD -i eth0 -o eth1 -s 192.168.0.20 -d 192.168.1.30 -p tcp --dport 22 -j ACCEPT
 
 # Allow SSH connections from database to app server
-sudo iptables -A FORWARD -i eth0 -o eth1 -s 192.168.1.30 -d 192.168.0.20 -p tcp --dport 22 -j ACCEPT
+sudo /sbin/iptables -A FORWARD -i eth0 -o eth1 -s 192.168.1.30 -d 192.168.0.20 -p tcp --dport 22 -j ACCEPT
 
 # Drop all other incoming and forwarding traffic
-sudo iptables -A INPUT -j DROP
-sudo iptables -A FORWARD -j DROP
+sudo /sbin/iptables -A INPUT -j DROP
+sudo /sbin/iptables -A FORWARD -j DROP
 
 # Make iptables persistent
 sudo apt install iptables-persistent -y
@@ -59,3 +56,6 @@ sudo sh -c 'ip6tables-save > /etc/iptables/rules.v6'
 
 # Enable netfilter-persisten.service to automatically apply iptables rules on boot
 sudo systemctl enable netfilter-persistent.service
+
+# Restart to apply changes
+sudo systemctl restart NetworkManager
