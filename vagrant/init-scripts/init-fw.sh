626699a1c6ee3c7d19d43efd7fe4e625aa977192
tildeeine <t1ld3me@gmail.com>
2023-12-18 19:53:05 +0000

    fix: Moved to ufw instead of iptables for simpler firewall setup

--- vagrant/init-scripts/init-fw.sh
+++ vagrant/init-scripts/init-fw.sh
@@ -3,56 +3,49 @@
 # VMSETTINGS: Adapter1, internal network, sw-1, promiscuous mode allow VMs. Adapter2, internal network, sw-2, promiscuous mode allow VMs. Adapter3, NAT. 
 
 # Configure the network interfaces
-sudo ifconfig eth0 192.168.0.10/24 up # App server
-sudo ifconfig eth1 192.168.1.19/24 up # Database server
+sudo ip addr add 192.168.0.10/24 dev eth0 # App server
+sudo ip addr add 192.168.1.20/24 dev eth1 # Database server
+sudo ip link set dev eth0 up
+sudo ip link set dev eth1 up
+
 
 # Activate IP forwarding
 sudo sysctl net.ipv4.ip_forward=1
 
 # Set up NAT to let AS access the internet
-sudo /sbin/iptables -t nat -F            # Flushes all the rules from table NAT
-sudo /sbin/iptables -t nat -A POSTROUTING  -o eth2 -j MASQUERADE    # Creates a source NAT on interface eth2
+sudo iptables -t nat -F            # Flushes all the rules from table NAT
+sudo iptables -t nat -A POSTROUTING  -o eth2 -j MASQUERADE    # Creates a source NAT on interface eth2
 
 # ! edit the corresponding /etc/network/interfaces file.
 
 #! ou should also enable IP forwarding permanently on VM2. For that you need to edit /etc/sysctl.conf and uncomment the following line: net.ipv4.ip_forward=1
 
 # Set up firewall rules
-# Flush existing rules and set the default policies
-sudo /sbin/iptables -F
-sudo /sbin/iptables -P INPUT DROP
-sudo /sbin/iptables -P FORWARD DROP
-sudo /sbin/iptables -P OUTPUT ACCEPT
+# Enable ufw to start on boot, add default rules, and deny all incoming traffic
+sudo ufw --force reset
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
 
 # Allow loopback interface
-sudo /sbin/iptables -A INPUT -i lo -j ACCEPT
-sudo /sbin/iptables -A OUTPUT -o lo -j ACCEPT
+sudo ufw allow in on lo
+sudo ufw allow out on lo
 
-# Allow SSH and HTTP connections from external machines (Auth and Client)
-sudo /sbin/iptables -A INPUT -i eth2 -p tcp --dport 22 -j ACCEPT
-sudo /sbin/iptables -A INPUT -i eth2 -p tcp --dport 80 -j ACCEPT
+# Allow HTTP connections from external machines (Auth and Client)
+sudo ufw allow in on eth2 to any port 80
 
-# Redirect all HTTP connections to the App server
-sudo /sbin/iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 192.168.0.20:80
+# Redirect all HTTP connections to the AS
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 192.168.0.20:80
 
-# Allow SSH connections from App server to the database
-sudo /sbin/iptables -A FORWARD -i eth0 -o eth1 -s 192.168.0.20 -d 192.168.1.30 -p tcp --dport 22 -j ACCEPT
+# Allow TLS/SSL connections from AS to the DB
+sudo ufw allow in on eth0 from 192.168.0.20 to 192.168.1.30 port 443
 
-# Allow SSH connections from database to app server
-sudo /sbin/iptables -A FORWARD -i eth0 -o eth1 -s 192.168.1.30 -d 192.168.0.20 -p tcp --dport 22 -j ACCEPT
+# Allow TLS/SSL connections from DB to AS
+sudo ufw allow in on eth1 from 192.168.1.30 to 192.168.0.20 port 443
 
 # Drop all other incoming and forwarding traffic
-sudo /sbin/iptables -A INPUT -j DROP
-sudo /sbin/iptables -A FORWARD -j DROP
-
-# Save current iptables rules
-# FOR IPv4
-sudo sh -c 'iptables-save > /etc/iptables/rules.v4'
-# FOR IPv6
-sudo sh -c 'ip6tables-save > /etc/iptables/rules.v6'
-
-# Enable netfilter-persisten.service to automatically apply iptables rules on boot
-sudo systemctl enable netfilter-persistent.service
+sudo ufw deny in on eth0
+sudo ufw deny in on eth1
 
 # Restart to apply changes
 sudo systemctl restart NetworkManager
