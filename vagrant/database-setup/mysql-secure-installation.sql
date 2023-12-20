229b970d97c7e5a35e13edbf280b559ee5fe5ea5
tildeeine <t1ld3me@gmail.com>
2023-12-20 03:16:08 +0000

    fix: firewall changed to iptables, NAT set up, added cert for db

--- vagrant/database-setup/mysql-secure-installation.sql
+++
@@ -1,9 +0,0 @@
--- mysql_secure_installation.sql
--- Automatically set the MySQL root password and remove anonymous users, disallow root login remotely, and remove the test database.
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'a31-MediTrack';
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-FLUSH PRIVILEGES;
--- vagrant/init-scripts/init-db-server.sh
+++ vagrant/init-scripts/init-db-server.sh
@@ -6,3 +6,14 @@ iface eth0 inet static
         address 192.168.0.20
         netmask 255.255.255.0
 " | sudo tee -a /etc/network/interfaces
+
+### Allow remote connections 
+# Edit /etc/mysql/mysql.conf.d/mysqld.cnf file   
+echo "[mysqld]
+ssl_ca=/MediTrack/keys/meditrack-server.crt
+ssl_cert=/MediTrack/keys/database-server.crt 
+ssl_key=/MediTrack/keys/database-server.priv.pem
+require_secure_transport=ON
+bind-address = 192.168.1.30
+" | sudo tee -a /etc/mysql/mysql.conf.d/mysqld.cnf
+
